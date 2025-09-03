7867d411978a65cb509d995cd4b3643c72b32750
MatteoFalcioni <matteo.falk@gmail.com>
2025-09-03 11:40:00 +0200

    best i can do with this voice!

--- T2S_S2T/speech_2_text.py
+++ T2S_S2T/speech_2_text.py
@@ -2,6 +2,7 @@ from elevenlabs import play, VoiceSettings
 from elevenlabs.client import ElevenLabs
 import os
 from dotenv import load_dotenv
+import re  
 
 from TARS.state import TARSState
 
@@ -9,6 +10,25 @@ load_dotenv()
 
 # Initialize ElevenLabs client
 elevenlabs_client = ElevenLabs(api_key=os.getenv("XI_API_KEY"))
+
+def add_tars_pauses(text):
+    """
+    Adds SSML break tags for pauses after sentence-ending punctuation (. and ?).
+    This creates TARS's characteristic deliberate pacing.
+    Returns the text wrapped in <speak> tags.
+    """
+    # Use regex to find all periods and question marks, and add a break tag after them.
+    # substitute "..." with a long break tag
+    text = re.sub(r'\.{3}', '<break time="0.6s"/>', text)
+    # The (?!\d) is a "negative lookahead" to avoid matching decimals like "90.5"
+    text = re.sub(r'([.?:;!])(?!\d)\s*', r'\1<break time="0.4s"/> ', text)
+    # add short pauses as well
+    text_with_pauses = re.sub(r'([—])(?!\d)\s*', r'\1<break time="0.1s"/> ', text)
+    
+    # WRAP THE FINAL TEXT IN <speak> TAGS
+    # This is what tells ElevenLabs to parse the content as SSML.
+    ssml_text = f"<speak>{text_with_pauses}</speak>"
+    return ssml_text
 
 def play_audio(state: TARSState):
     
@@ -19,17 +39,20 @@ def play_audio(state: TARSState):
 
     # Prepare text by replacing ** with empty strings
     # These can cause unexpected behavior in ElevenLabs
-    cleaned_text = response.content.replace("**", "")
+    cleaned_text = response.content.replace("**", "").replace("$", "").replace("/", "" )
+
+    text_with_ssml = add_tars_pauses(cleaned_text)
+    print(text_with_ssml)
     
     # Call text_to_speech API with turbo model for low latency
     response = elevenlabs_client.text_to_speech.convert(
         voice_id="dNXy174F4uFM8G0CUjYL", # Adam pre-made voice
         output_format="mp3_22050_32",
-        text=cleaned_text,
+        text=text_with_ssml,
         model_id="eleven_turbo_v2_5", 
         voice_settings=VoiceSettings(
-            stability=0.8,         # a bit less flat → slight inflection
-            similarity_boost=0.95,  # lean into the cloned timbre
+            stability=0.78,         # a bit less flat → slight inflection
+            similarity_boost=0.92,  # lean into the cloned timbre
             style=0.1,             # tiny expressiveness
             use_speaker_boost=False # avoid cinematic boom
         )
@@ -37,3 +60,6 @@ def play_audio(state: TARSState):
     
     # Play the audio back
     play(response)
+
+
+
